e6eaa3f6d2450b1afdd621b2b49ea8c03e8aa36b
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:24:34 +0200

    User validation and such

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿//küsib kasutaja vanust
+//kui kasutaja vanus on 13 siis on kasutaja liiga noor instagrammi jaoks
+//üle 13 aastasele kuvab tere tulemast instagrammi
+
+Console.WriteLine("Enter your age:");
+string userAge = Console.ReadLine(); //kasutaja vanuse sisend
+int userAgeNum = 0;
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+if (isAgeNumber)
+{
+    if (userAgeNum >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram!");
+    }
+}
+else
+{
+    Console.WriteLine("Could not read your age.");
+}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,20 @@
+﻿//küsib kasutaja sugu (m/f)
+//küsib kasutaja perekonnanime
+//tervitab kasutajat valitud soo ja perekonnanimega
+
+Console.WriteLine("Please, select your gender (M/F)");
+char userGender = Char.Parse(Console.ReadLine()); //kasutaja soo sisend
+Console.WriteLine("Please enter your last name"); //kasutaja perekonnanime sisend
+string userLastName = Console.ReadLine();
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
